a0f522f55259210d3247ff5a122707e691b825cc
dollyrestieog-wq <dollyrestieog@gmail.com>
2026-06-21 00:03:41 +0300

    Update MeetingRoom.tsx

--- src/components/features/MeetingRoom.tsx
+++ src/components/features/MeetingRoom.tsx
@@ -12,12 +12,29 @@ import { toast } from 'sonner';
 
 const RTC_CONFIG: RTCConfiguration = {
   iceServers: [
-    { urls: 'stun:stun.l.google.com:19302' },
-    { urls: 'stun:stun1.l.google.com:19302' },
-    { urls: 'stun:stun2.l.google.com:19302' },
-    { urls: 'stun:stun.cloudflare.com:3478' },
+    {
+      urls: [
+        'stun:stun.l.google.com:19302',
+        'stun:stun1.l.google.com:19302',
+      ],
+    },
+
+    {
+      urls: 'turn:openrelay.metered.ca:80',
+      username: 'openrelayproject',
+      credential: 'openrelayproject',
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:443',
+      username: 'openrelayproject',
+      credential: 'openrelayproject',
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+      username: 'openrelayproject',
+      credential: 'openrelayproject',
+    },
   ],
-  iceCandidatePoolSize: 10,
 };
 
 function formatDuration(secs: number) {
